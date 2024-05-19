764cc8c4554bf1984cc63dd162d2d99431136038
rmahotka <rmahotka@gmail.com>
2024-05-19 23:01:14 +0300

    add components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            key={index}
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
