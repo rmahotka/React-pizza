799fc0218203b3807442ae16aa717b6239631187
rmahotka <rmahotka@gmail.com>
2024-05-27 23:08:23 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch(`https://664b94a935bbda10987d7816.mockapi.io/items`)
-      .then((response) => response.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <NotFound />
         </div>
       </div>
     </div>
